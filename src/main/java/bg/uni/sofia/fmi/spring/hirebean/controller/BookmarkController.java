f4e9079fe1d7b3f386af0d04d6bb87068678d0cc
Serhan Chavdarliev <serhi1334@gmail.com>
2026-03-13 03:34:50 +0200

    add BookmarkController.java

---
+++ src/main/java/bg/uni/sofia/fmi/spring/hirebean/controller/BookmarkController.java
@@ -0,0 +1,49 @@
+package bg.uni.sofia.fmi.spring.hirebean.controller;
+
+import bg.uni.sofia.fmi.spring.hirebean.dto.response.JobOfferResponse;
+import bg.uni.sofia.fmi.spring.hirebean.service.BookmarkService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/bookmarks")
+@RequiredArgsConstructor
+public class BookmarkController {
+
+    private final BookmarkService bookmarkService;
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<JobOfferResponse>> getBookmarks(
+        @PathVariable Long userId
+    ) {
+        return ResponseEntity.ok(bookmarkService.getBookmarsByUserId(userId));
+    }
+
+    @PostMapping("/user/{userId}/job/{jobOfferId}")
+    public ResponseEntity<Void> addBookmark(
+        @PathVariable Long userId,
+        @PathVariable Long jobOfferId
+    ) {
+        bookmarkService.addBookmark(userId, jobOfferId);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/user/{userId}/job/{jobOfferId}")
+    public ResponseEntity<Void> removeBookmark(
+        @PathVariable Long userId,
+        @PathVariable Long jobOfferId
+    ) {
+        bookmarkService.removeBookmark(userId, jobOfferId);
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
